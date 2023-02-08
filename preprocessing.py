8adf828599d9a8ab0395bfeb452381c89bec691e
bernadette <b.j.mohr@uva.nl>
2023-02-08 15:25:38 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,22 +6,19 @@ class PreprocessPipeline:
     def __init__(self, coordinates, trajectory):
         self.universe = mda.Universe(coordinates, trajectory, in_memory=True)
 
-    def __del__(self):
-        class_name = self.__class__.__name__
-        print(f'{class_name} deleted')
-
     def preprocess_sim_data(self) -> object:
         """
-        Preprocesses all frames in simulation and returns TODO
-
+        Preprocesses all frames in simulation and returns a list of selected particles and a list of their respective
+         xyz coordinates. Particles are selected accoridng to the cut-off distance defined by the long-range
+         interactions of the simulation around the COM of a central solute.
         """
         universe = self.universe
-
+        print('Extracting relevant particles...')
         sel = universe.select_atoms('(resname MOL or around 11 resname MOL) and '
                                     'not (name WP or name WM or name NAP or name NAM)', updating=True)
-        # mol = universe.select_atoms('resname MOL', updating=True)
-
-        # list of particles, list of particle positions per frame
+        """
+         list of partilces, list of particle positions per frame
+        """
         sel_positions, sel_atoms = list(), list()
         for timestep in universe.trajectory:
             sel_atoms.append(sel.atoms)
