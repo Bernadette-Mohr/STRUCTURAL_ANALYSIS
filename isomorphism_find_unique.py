80480023a84ec834476f63ff6891617e17a3bb40
bernadette <b.j.mohr@uva.nl>
2023-02-08 15:27:11 +0100

    Update isomorphism_find_unique.py

--- isomorphism_find_unique.py
+++ isomorphism_find_unique.py
@@ -1,22 +1,12 @@
-import sys
 import argparse
 from pathlib import Path
 import pickle
 import networkx as nx
-import networkx.algorithms.isomorphism as iso
 
 
 def clean_graph(graph):
     graph.remove_edges_from(nx.selfloop_edges(graph))
     return graph
-
-
-# def filter_value(unique_graphs, graph):
-#     for idx, element in enumerate(unique_graphs):
-#         if nx.is_isomorphic(element[0], graph):
-#             element[1] = element[1] + 1
-#             print(element)
-#             unique_graphs[idx] = element
 
 
 def find_isomorphisms(unique_graphs, graphs):
@@ -25,15 +15,11 @@ def find_isomorphisms(unique_graphs, graphs):
         is_isomer = False
         for idx, element in enumerate(unique_graphs):
             if nx.is_isomorphic(element[0], graph):
-                # GM = iso.GraphMatcher(element[0], graph)
-                # print(GM.is_isomorphic())
-                # print(GM.mapping)
                 number = element[1]
                 number += 1
                 unique_graphs[idx] = (element[0], number)
                 is_isomer = True
                 break
-        # is_isomer = [True for unique in unique_graphs if nx.is_isomorphic(graph, unique)]
         if not is_isomer:
             unique_graphs.append((graph, 1))
 
@@ -43,17 +29,16 @@ def find_isomorphisms(unique_graphs, graphs):
 def process_graphs(dirpath):
 
     unique_graphs = list()
-    # for rnd_graphs in sorted(dirpath.glob('**/**/*.pkl')):  # ROUND_*/graphs/*.pkl
-    rnd_graphs = dirpath / 'graphs_with_FE-results.pkl'
-    with open(rnd_graphs, 'rb') as graphs_pkl:
-        print(rnd_graphs)
-        graphs = pickle.load(graphs_pkl)
-        if not unique_graphs:
-            unique_graphs.append((clean_graph(graphs[0]), 1))
-        unique_graphs = find_isomorphisms(unique_graphs, graphs)
+    for rnd_graphs in sorted(dirpath.glob('**/**/*.pkl')):  # ROUND_*/graphs/*.pkl
+        with open(rnd_graphs, 'rb') as graphs_pkl:
+            print(rnd_graphs)
+            graphs = pickle.load(graphs_pkl)
+            if not unique_graphs:
+                unique_graphs.append((clean_graph(graphs[0]), 1))
+            unique_graphs = find_isomorphisms(unique_graphs, graphs)
 
     print('# unique structures:', len(unique_graphs))
-    save_path = dirpath / 'unique_graphs_FE-results_freq.pkl'
+    save_path = dirpath / 'unique_graphs_freq.pkl'
     with open(save_path, 'wb') as uniques_pkl:
         pickle.dump(unique_graphs, uniques_pkl)
 
